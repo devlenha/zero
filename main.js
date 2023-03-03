6ea66f099ed55a1c5b1b49a1b7acf0a0e578d859
Alysson Machao <alylenhadev@gmail.com>
2023-03-03 10:41:07 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+function modeLigth(){
+  const dom = document.body
+  /*Tira e coloca class */
+  dom.classList.toggle("ligth")
+
+  /*
+  if(dom.classList.contains('ligth')){
+    dom.classList.remove('ligth')
+  }
+  else{
+    dom.classList.add('ligth')
+  }
+
+*/
+  //Substia a img
+  const img = document.querySelector('#profile img');
+
+  if(dom.classList.contains('ligth')){
+    img.setAttribute('src',"./assets/img_profile_dark.png")
+    img.setAttribute("alt", "TESTE")
+  }else{
+img.setAttribute("src", "./assets/img_profile.png")
+img.setAttribute("alt", "volou")
+  }
+  
+  
+
+}
+
+
